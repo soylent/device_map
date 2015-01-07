c6abfe9b387e6fa3db9dc0912db835d70b798112
Konstantin Papkovskiy <konstantin@papkovskiy.com>
2015-01-07 10:41:42 +0300

    Refactor devices class

--- lib/device_map/openddr/devices.rb
+++ lib/device_map/openddr/devices.rb
@@ -1,18 +1,28 @@
 module DeviceMap
   module OpenDDR
     class Devices
-      def initialize(devices_xml)
-        @devices = {}
+      class DeviceNotFound < StandardError; end
 
+      def self.parse(devices_xml)
         devices_doc = Nokogiri::XML(devices_xml)
-        devices_doc.xpath('//device').each do |device_node|
-          @devices[device_node[:id]] = Device.parse(device_node)
+        devices = devices_doc.xpath('//device').map do |device_node|
+          Device.parse(device_node)
+        end
+
+        new(devices)
+      end
+
+      def initialize(devices)
+        @device_index = devices.each_with_object({}) do |device, device_index|
+          device_index[device.id] = device
         end
       end
 
       def find(device_id)
         # TODO: Return copy of <tt>Device</tt> object
-        @devices[device_id]
+        @device_index.fetch(device_id) do
+          fail DeviceNotFound, "Cound not find device: #{device_id}"
+        end
       end
     end
   end
--- spec/openddr/devices_spec.rb
+++ spec/openddr/devices_spec.rb
@@ -3,19 +3,33 @@ require 'device_map'
 RSpec.describe DeviceMap::OpenDDR::Devices do
   describe '#find' do
     it 'finds device by its id' do
-      device_id = 'device_id'
-      devices_doc = generate_devices(device_id)
-      devices = described_class.new(devices_doc.to_xml)
-      device = devices.find(device_id)
-      expect(device.id).to eq device_id
+      device_stub = double(id: 'device_id')
+      devices = described_class.new(Array(device_stub))
+      device = devices.find(device_stub.id)
+      expect(device.id).to eq device_stub.id
+    end
+
+    it 'raises exception if device is not found' do
+      devices = described_class.new([])
+      expect do
+        devices.find('anything')
+      end.to raise_error(DeviceMap::OpenDDR::Devices::DeviceNotFound)
     end
   end
 
-  def generate_devices(device_id)
+  describe '.parse' do
+    it 'returns new instance of this class' do
+      devices_xml = generate_devices_xml.to_xml
+      devices = described_class.parse(devices_xml)
+      expect(devices).to be_a(described_class)
+    end
+  end
+
+  def generate_devices_xml
     Nokogiri::XML::Builder.new do |xml|
       xml.ODDR do
         xml.Devices do
-          xml.device(id: device_id)
+          xml.device
         end
       end
     end
