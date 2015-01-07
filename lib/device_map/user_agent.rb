1044f69848aa38a42f0f19ea10617a0134eaa070
Konstantin Papkovskiy <konstantin@papkovskiy.com>
2015-01-07 12:05:44 +0300

    Generate ngrams from user-agent keywords

--- lib/device_map/user_agent.rb
+++ lib/device_map/user_agent.rb
@@ -4,8 +4,15 @@ module DeviceMap
       @user_agent = user_agent
     end
 
-    def keywords
-      @user_agent.downcase.split(/[\s;\/\(\)]+/)
+    def ngrams(size)
+      keywords = @user_agent.downcase.split(/[\s;\/\(\)]+/)
+
+      keywords.flat_map.with_index do |keyword, i|
+        (size + 1).times.map do |j|
+          next_keywords = keywords[i + 1..-1] || []
+          Array(keyword).concat next_keywords.take(j)
+        end
+      end.uniq
     end
   end
 end
--- spec/user_agent_spec.rb
+++ spec/user_agent_spec.rb
@@ -1,30 +1,46 @@
 require 'device_map'
 
 RSpec.describe DeviceMap::UserAgent do
-  describe '#keywords' do
+  describe '#ngrams' do
     example do
       user_agent = described_class.new('iphone')
-      expect(user_agent.keywords).to eq ['iphone']
-    end
+      ngrams = user_agent.ngrams(1)
 
-    example do
-      user_agent = described_class.new('iPhone')
-      expect(user_agent.keywords).to eq ['iphone']
+      expect(ngrams.size).to eq 1
+      expect(ngrams).to include ['iphone']
     end
 
     example do
       user_agent = described_class.new('iPhone; U')
-      expect(user_agent.keywords).to eq ['iphone', 'u']
+      ngrams = user_agent.ngrams(2)
+
+      expect(ngrams.size).to eq 3
+      expect(ngrams).to include ['iphone']
+      expect(ngrams).to include ['iphone', 'u']
+      expect(ngrams).to include ['u']
     end
 
     example do
       user_agent = described_class.new('Mozilla/5.0')
-      expect(user_agent.keywords).to eq ['mozilla', '5.0']
+      ngrams = user_agent.ngrams(3)
+
+      expect(ngrams.size).to eq 3
+      expect(ngrams).to include ['mozilla']
+      expect(ngrams).to include ['mozilla', '5.0']
+      expect(ngrams).to include ['5.0']
     end
 
     example do
       user_agent = described_class.new('Mozilla/5.0 (iPhone)')
-      expect(user_agent.keywords).to eq ['mozilla', '5.0', 'iphone']
+      ngrams = user_agent.ngrams(2)
+
+      expect(ngrams.size).to eq 6
+      expect(ngrams).to include ['mozilla']
+      expect(ngrams).to include ['mozilla', '5.0']
+      expect(ngrams).to include ['mozilla', '5.0', 'iphone']
+      expect(ngrams).to include ['5.0']
+      expect(ngrams).to include ['5.0', 'iphone']
+      expect(ngrams).to include ['iphone']
     end
   end
 end
