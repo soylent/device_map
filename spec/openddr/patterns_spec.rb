c52f5cb81ef5f304ec4270c5002f2ea9251163b6
Konstantin Papkovskiy <konstantin@papkovskiy.com>
2015-01-09 14:52:49 +0300

    Update specs for pattern index

--- spec/openddr/patterns_spec.rb
+++ spec/openddr/patterns_spec.rb
@@ -1,15 +1,17 @@
 require 'device_map'
 
 RSpec.describe DeviceMap::OpenDDR::Patterns do
+  let(:keyword) { 'keyword' }
+
+  let(:pattern) do
+    DeviceMap::Pattern.new(keyword, 'device_id', 1)
+  end
+
   # FIXME: Bad specs
   describe '.parse' do
-    it 'retuns instance of patterns class' do
-      pattern = DeviceMap::Pattern.new('anything', 'anything', 1)
+    it 'returns instance of patterns class' do
       builder_stub = double(:builder, patterns: Array(pattern))
-
-      expect(DeviceMap::OpenDDR::Builder).to receive(:find) do
-        builder_stub
-      end
+      expect(DeviceMap::OpenDDR::Builder).to receive(:find) { builder_stub }
 
       openddr_builder = Nokogiri::XML::Builder.new do |xml|
         xml.ODDR do
@@ -26,24 +28,18 @@ RSpec.describe DeviceMap::OpenDDR::Patterns do
       end
 
       patterns = described_class.parse(openddr_builder.to_xml)
+
       expect(patterns).to be_a(described_class)
+      expect(patterns.find(keyword)).to include pattern
     end
   end
 
   describe '#find' do
     it 'returns list of patterns for the given keyword' do
-      device_id, keyword = 'iphone', 'ios'
-      pattern = DeviceMap::Pattern.new(keyword, device_id, 1)
-
       patterns = described_class.new(Array(pattern))
       search_results = patterns.find(keyword)
 
-      expect(search_results).not_to be_empty
-
-      search_results.each do |pattern|
-        expect(pattern.device_id).to eq device_id
-        expect(pattern.keywords).to include keyword
-      end
+      expect(search_results).to include pattern
     end
 
     it 'returns empty list if keyword is not found' do
