ee61f366fbd750efc7b96214309f5b46280b7686
Konstantin Papkovskiy <konstantin@papkovskiy.com>
2015-01-09 09:52:38 +0300

    Update keyword normalization function

--- lib/device_map/keyword.rb
+++ lib/device_map/keyword.rb
@@ -1,9 +1,17 @@
 module DeviceMap
   module Keyword
+    # Deletes all non-alphanumeric characters from the given keywords
     def self.normalize(keywords)
       keywords.map do |keyword|
-        # FIXME: Problem with the blackberry keywords in the XML files
-        keyword.downcase.tr('[bb]', 'b').gsub(/\W+/, '')
+        # NOTE: <tt>BUILDER_DATA_SOURCE</tt> contains keywords like:
+        #     <device id="BlackBerry 9650">
+        #         <list>
+        #             <value>[Bb]lack.?[Bb]erry</value>
+        #             <value>blackberry 9650</value>
+        #         </list>
+        #     </device>
+        # In such cases we want to replace patterns with simple keywords.
+        keyword.downcase.gsub('[bb]', 'b').gsub(/[\W_]+/, '')
       end
     end
   end
