854bb9420104e56e143f3b90bde6c6945e7d6890
Konstantin Papkovskiy <konstantin@papkovskiy.com>
2015-01-09 14:14:01 +0300

    Pattern can check if it matches against a list of keywords

--- spec/pattern_spec.rb
+++ spec/pattern_spec.rb
@@ -1,28 +1,49 @@
 require 'device_map'
 
 RSpec.describe DeviceMap::Pattern do
+  let(:priority) { 1 }
+  let(:higher_priority) { priority + 1 }
+
+  let(:keyword) { 'keyword' }
+  let(:longer_keyword) { 'longer_keyword' }
+
+  let(:device_id) { 'device_id' }
+
   it 'is comparable by keyword size' do
-    pattern1 = described_class.new('keyword', 'device1', 1)
-    pattern2 = described_class.new('long_keyword', 'device2', 1)
+    pattern1 = described_class.new(keyword, device_id, priority)
+    pattern2 = described_class.new(longer_keyword, device_id, priority)
 
-    patterns = [pattern1, pattern2]
-
-    expect(patterns.max).to eq pattern2
+    expect(pattern1).to be < pattern2
   end
 
   it 'is comparable by priority' do
-    pattern1 = described_class.new('long_keyword', 'device1', 1)
-    pattern2 = described_class.new('keyword', 'device2', 2)
+    pattern1 = described_class.new(keyword, device_id, higher_priority)
+    pattern2 = described_class.new(longer_keyword, device_id, priority)
 
-    patterns = [pattern1, pattern2]
-
-    expect(patterns.max).to eq pattern2
+    expect(pattern1).to be > pattern2
   end
 
   it 'can be equal to other pattern' do
-    pattern1 = described_class.new('keyword', 'device', 1)
-    pattern2 = described_class.new('keyword', 'device', 1)
+    pattern1 = described_class.new(keyword, device_id, priority)
+    pattern2 = described_class.new(keyword, device_id, priority)
 
     expect(pattern1).to eq pattern2
+  end
+
+  describe '#matches?' do
+    let(:pattern) do
+      described_class.new(keyword, device_id, priority)
+    end
+
+    let(:other_keywords) { ['anything', 'else'] }
+
+    it 'returns true if given keywords contain all pattern keywords' do
+      given_keywords = other_keywords + pattern.keywords
+      expect(pattern.matches?(given_keywords)).to eq true
+    end
+
+    it 'returns false if given keywords do not contain all pattern keywords' do
+      expect(pattern.matches?(other_keywords)).to eq false
+    end
   end
 end
